30def94679f1c394b1d36fd6e1ffff15748f00e6
lihuajun <2950925006@qq.com>
2020-11-22 00:08:07 +0800

    123

---
+++ src/main/java/com/lee/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,21 @@
+package com.lee.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+    @RequestMapping
+    public Object demo(){
+        return "jekins-demo";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+}
